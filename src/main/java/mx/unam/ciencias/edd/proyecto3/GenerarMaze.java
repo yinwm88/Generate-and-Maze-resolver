62d3eed9fdd0361a84efd2619ebff8a6c69fc778
wong <yinlen@ciencias.unam.mx>
2024-05-17 09:32:07 -0600

    ya se leen las banderas y se manda errores si hay

--- src/main/java/mx/unam/ciencias/edd/proyecto3/GenerarMaze.java
+++ src/main/java/mx/unam/ciencias/edd/proyecto3/GenerarMaze.java
@@ -3,20 +3,28 @@ package mx.unam.ciencias.edd.proyecto3;
 import mx.unam.ciencias.edd.*;
 import java.util.Random;
 
+/**
+ * Clase para generar un archivo que contiene un laberinto valido.
+ */
 public class GenerarMaze {
 
-    // Banderas
+    /**Bandera permite generar un laberinto*/
     private static boolean g = false;
+    /**Bandera permite saber las columnas*/
     private static boolean w = false;
+    /**Bandera permite saber los renglones*/
     private static boolean h = false;
+    /**Bandera opcional permite bindar una semilla*/
     private static boolean s = false;
 
-    // Valores del laberinto
+    /**Numero de columnas*/
     private static int columnas;
+    /**Numero de renglones*/
     private static int renglones;
+    /**Valor de la semilla*/
     private static int semilla;
 
-    // Verifica las banderas
+    /**Metood para verificar la existencia de banderas*/
     private static boolean checkBanderas(String[] entrada) {
         for (String b : entrada) {
             if (b.equals("-g")) g = true;
@@ -27,7 +35,16 @@ public class GenerarMaze {
         return g && w && h;
     }
 
-    // Obtiene los valores de los argumentos
+    /**Imprime el uso correcto del programa*/
+    private static void printUsage() {
+        System.out.println("\nAsegúrate de incluir las siguientes banderas:");
+        System.out.println(" '-g' obligatoria para generar un laberinto.");
+        System.out.println(" '-w' obligatoria para indicar el número de columnas del laberinto.");
+        System.out.println(" '-h' obligatoria para indicar el número de renglones del laberinto.");
+        System.out.println(" '-s' opcional si deseas agregar una semilla para generar el laberinto.\n");
+    }
+
+    /**Metodo para obtener los valores del laberinto*/
     public static void getValues(String[] entrada) {
         if (checkBanderas(entrada)) {
             for (int i = 0; i < entrada.length; i++) {
@@ -84,22 +101,10 @@ public class GenerarMaze {
         }
     }
 
-    /**
-     * Imprime el uso correcto del programa
-    */
-    private static void printUsage() {
-        System.out.println("\nAsegúrate de incluir las siguientes banderas:");
-        System.out.println(" '-g' obligatoria para generar un laberinto.");
-        System.out.println(" '-w' obligatoria para indicar el número de columnas del laberinto.");
-        System.out.println(" '-h' obligatoria para indicar el número de renglones del laberinto.");
-        System.out.println(" '-s' opcional si deseas agregar una semilla para generar el laberinto.\n");
-    }
-
-    // Genera el laberinto
+    /**Metodo para generar un laberinto */
     public static void generarMaze() {
         Random random = s ? new Random(semilla) : new Random();
         //Maze maze = new Maze(columnas, renglones, random);
-        // Aquí podrías tener el código para generar y mostrar el laberinto
     }
 
 }
