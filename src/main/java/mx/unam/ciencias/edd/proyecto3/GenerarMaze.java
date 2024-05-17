c313cd5fa69bc9bfcc6cb6f43ba6bd1a8e120ffe
wong <yinlen@ciencias.unam.mx>
2024-05-17 09:20:21 -0600

    ya se leen las banderas y se manda errores si hay

--- src/main/java/mx/unam/ciencias/edd/proyecto3/GenerarMaze.java
+++ src/main/java/mx/unam/ciencias/edd/proyecto3/GenerarMaze.java
@@ -1,115 +1,105 @@
 package mx.unam.ciencias.edd.proyecto3;
 
 import mx.unam.ciencias.edd.*;
-import java.io.PrintStream;
 import java.util.Random;
-
 
 public class GenerarMaze {
 
-    /* Bandera  permite generar un laberinto*/
-    private  static boolean g = false;
-    /* Bandera asigna columnas*/
+    // Banderas
+    private static boolean g = false;
     private static boolean w = false;
-    /* Bandera  asigna  renglones*/
     private static boolean h = false;
-    /* Bandera opcional para brindar semilla*/
     private static boolean s = false;
 
-    /* Las columnas del laberinto*/
+    // Valores del laberinto
     private static int columnas;
-    /* Los renglones del laberinto*/
-    private static  int renglones;
-    /* Registro de semilla */
+    private static int renglones;
     private static int semilla;
 
-
-    private static  boolean checkBanderas(String[] entrada) {
+    // Verifica las banderas
+    private static boolean checkBanderas(String[] entrada) {
         for (String b : entrada) {
             if (b.equals("-g")) g = true;
             if (b.equals("-w")) w = true;
             if (b.equals("-h")) h = true;
             if (b.equals("-s")) s = true;
         }
-        return (g && w )&& h;
+        return g && w && h;
     }
 
-
+    // Obtiene los valores de los argumentos
     public static void getValues(String[] entrada) {
-        if(checkBanderas(entrada) == true){
+        if (checkBanderas(entrada)) {
             for (int i = 0; i < entrada.length; i++) {
                 switch (entrada[i]) {
                     case "-s":
-                        try {
-                            if((!entrada[i + 1].equals("-w")) && (!entrada[i + 1].equals("-g")) && (!entrada[i + 1].equals("-h"))){
+                        if (i + 1 < entrada.length && !entrada[i + 1].startsWith("-")) {
+                            try {
                                 semilla = Integer.parseInt(entrada[i + 1]);
                                 if (semilla <= 0) {
                                     System.out.println("El valor de la semilla debe ser un número positivo.");
                                 }
-                            }else{
-                                System.out.println("Asegúrate de incluir un valor para la semilla.");
-
+                            } catch (NumberFormatException e) {
+                                System.out.println("El valor de la semilla debe ser un número.");
                             }
-                        } catch (ArrayIndexOutOfBoundsException e) {
+                            i++;
+                        } else {
                             System.out.println("Asegúrate de incluir un valor para la semilla.");
-                        } catch (NumberFormatException e) {
-                            System.out.println("El valor de la semilla debe ser un número.");
-                        } catch (IllegalArgumentException e) {
-                            System.out.println(e.getMessage());
                         }
                         break;
                     case "-w":
-                        try {
-                            if((!entrada[i + 1].equals("-s")) && (!entrada[i + 1].equals("-g")) && (!entrada[i + 1].equals("-h"))){
+                        if (i + 1 < entrada.length && !entrada[i + 1].startsWith("-")) {
+                            try {
                                 columnas = Integer.parseInt(entrada[i + 1]);
                                 if (columnas <= 0) {
-                                    System.out.println("El valor del ancho debe ser un número positivo.");
+                                    System.out.println("El valor de las columnas debe ser un número positivo.");
                                 }
-                            }else{
-                                System.out.println("Asegúrate de incluir un valor para las columnas.");
-
+                            } catch (NumberFormatException e) {
+                                System.out.println("El valor de las columnas debe ser un número.");
                             }
-                        } catch (ArrayIndexOutOfBoundsException e) {
-                            System.out.println("Asegúrate de incluir un valor para el ancho.");
-                        } catch (NumberFormatException e) {
-                            System.out.println("El valor del ancho debe ser un número.");
-                        } catch (IllegalArgumentException e) {
-                            System.out.println(e.getMessage());
+                            i++;
+                        } else {
+                            System.out.println("Asegúrate de incluir un valor para las columnas.");
                         }
                         break;
                     case "-h":
-                        try {
-                            if((!entrada[i + 1].equals("-w")) && (!entrada[i + 1].equals("-g")) && (!entrada[i + 1].equals("-s"))){
+                        if (i + 1 < entrada.length && !entrada[i + 1].startsWith("-")) {
+                            try {
                                 renglones = Integer.parseInt(entrada[i + 1]);
                                 if (renglones <= 0) {
-                                    System.out.println("El valor de la altura debe ser un número positivo.");
-                                } 
-                            }else{
-                                System.out.println("Asegúrate de incluir un valor para los renglones.");
-
+                                    System.out.println("El valor de los renglonesdebe ser un número positivo.");
+                                }
+                            } catch (NumberFormatException e) {
+                                System.out.println("El valor de los renglones debe ser un número.");
                             }
-                        } catch (ArrayIndexOutOfBoundsException e) {
-                            System.out.println("Asegúrate de incluir un valor para la altura.");
-                        } catch (NumberFormatException e) {
-                            System.out.println("El valor de la altura debe ser un número.");
-                        } catch (IllegalArgumentException e) {
-                            System.out.println(e.getMessage());
+                            i++;
+                        } else {
+                            System.out.println("Asegúrate de incluir un valor para los renglones.");
                         }
                         break;
                 }
             }
-            if(!s){
-                Random rand = new Random();
-                semilla = rand.nextInt(100);
-            }
-        }else{
-            System.out.println("\nAsegurate de incluir las siguientes banderas:\n '-g' obligatoria para generar un laberinto.\n '-h' obligatoria para indicar el numero de renglones del laberinto.\n '-w' obligatoria para indicar el numero de columnas del laberinto.\n '-s' OPCIONAL por si deseas agregar una semilla para generar el laberinto.\n");
+        } else {
+            printUsage();
         }
     }
 
+    /**
+     * Imprime el uso correcto del programa
+    */
+    private static void printUsage() {
+        System.out.println("\nAsegúrate de incluir las siguientes banderas:");
+        System.out.println(" '-g' obligatoria para generar un laberinto.");
+        System.out.println(" '-w' obligatoria para indicar el número de columnas del laberinto.");
+        System.out.println(" '-h' obligatoria para indicar el número de renglones del laberinto.");
+        System.out.println(" '-s' opcional si deseas agregar una semilla para generar el laberinto.\n");
+    }
 
-
-
-
+    // Genera el laberinto
+    public static void generarMaze() {
+        Random random = s ? new Random(semilla) : new Random();
+        //Maze maze = new Maze(columnas, renglones, random);
+        // Aquí podrías tener el código para generar y mostrar el laberinto
+    }
 
 }
